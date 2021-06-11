5567998b82cf697cde1328fed653a91dbbf2d6ea
MejanH <muhammad.mejanul.haque@gmail.com>
2021-06-11 15:51:39 +0600

    blog rest crud api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import blogRouter from "./routes/blog";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(blogRouter);
 
--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -5,12 +5,44 @@ import { Post } from "../models/post";
 const blogRouter = express.Router();
 
 blogRouter.get("/blog", async (req: Request, res: Response) => {
-  const post: PostType = new Post({
-    title: "hello world",
-    description: "lorem  sdf ww er vwer wer",
-  });
-  await post.save();
   return res.send("all posts");
+});
+
+blogRouter.post("/blog/new", async (req: Request, res: Response) => {
+  const post: PostType = new Post(req.body);
+  await post
+    .save()
+    .then(() => {
+      res.status(200).send("Your Blog Post Created Successfully.");
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
+
+blogRouter.get("/blog/:id", async (req: Request, res: Response) => {
+  const ID = req.params.id;
+  await Post.findById(ID)
+    .then((post) => {
+      res.status(200).send(post);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
+
+blogRouter.put("/blog/edit/:id", async (req: Request, res: Response) => {
+  const ID = req.params.id;
+  Post.updateOne({ _id: ID }, req.body)
+    .then(() => res.status(200).send("Post udpated successfully"))
+    .catch((err) => res.status(400).send(err));
+});
+
+blogRouter.delete("/blog/delete/:id", async (req: Request, res: Response) => {
+  const ID = req.params.id;
+  Post.findByIdAndDelete(ID)
+    .then(() => res.status(200).send("Deleted Successfully"))
+    .catch((err) => res.status(400).send(err));
 });
 
 export default blogRouter;
